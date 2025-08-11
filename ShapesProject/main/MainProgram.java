03cfb1e7257731ee610fc59667c9be41cef4204b
navee006 <naveenarubythomas@gmail.com>
2025-08-11 21:50:32 +0530

    Add files via upload

---
+++ ShapesProject/main/MainProgram.java
@@ -0,0 +1,10 @@
+package main;
+
+import shapes.ShapeDisplay;
+
+public class MainProgram {
+    public static void main(String[] args) {
+        ShapeDisplay sd = new ShapeDisplay();
+        sd.display(); 
+    }
+}
---
+++ ShapesProject/shapes/ShapeDisplay.java
@@ -0,0 +1,11 @@
+package shapes;
+
+public class ShapeDisplay {
+
+    // Single method that prints all shapes
+    public void display() {
+        System.out.println("Displaying a circle.");
+        System.out.println("Displaying a square.");
+        System.out.println("Displaying a triangle.");
+    }
+}
