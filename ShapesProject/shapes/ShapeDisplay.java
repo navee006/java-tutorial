d88ac83ac5495219ed75478d9632248a8cdb0ce8
navee006 <naveenarubythomas@gmail.com>
2025-08-11 22:18:59 +0530

    package

--- ShapesProject/shapes/ShapeDisplay.java
+++ ShapesProject/shapes/ShapeDisplay.java
@@ -1,11 +1,10 @@
 package shapes;
 
 public class ShapeDisplay {
-
-    // Single method that prints all shapes
     public void display() {
         System.out.println("Displaying a circle.");
         System.out.println("Displaying a square.");
         System.out.println("Displaying a triangle.");
     }
 }
+
